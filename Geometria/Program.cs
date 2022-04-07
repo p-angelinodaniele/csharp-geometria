b4fce25dcf989fa81f91907d6aedf941ffacf3f4
P.angelino <p.angelinodaniele@gmail.com>
2022-04-07 15:10:07 +0200

    Add project files.

---
+++ Geometria/Program.cs
@@ -0,0 +1,17 @@
+﻿
+
+Console.Write("Inserisci il nome del rettangolo: ");
+string nomeRettangolo = (Console.ReadLine());
+Console.Write("Inserisci la base del rettangolo: ");
+int baseRettangolo = int.Parse(Console.ReadLine());
+Console.Write("Inserisci l'altezza del rettangolo: ");
+int altezzaRettangolo = int.Parse(Console.ReadLine());
+
+
+
+
+Rettangolo r1 = new Rettangolo(nomeRettangolo ,baseRettangolo, altezzaRettangolo);
+
+r1.CalcolaArea();
+r1.CalcolaPerimetro();
+r1.stampaRettangolo();
---
+++ Geometria/Rettangolo.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+    public class Rettangolo
+    {
+        private string nomeRettangolo;
+        private int baseRettangolo;
+        private int altezzaRettangolo;
+
+
+        public Rettangolo(string nomeRettangolo ,int baseRettangolo, int altezzaRettangolo)
+        {
+            this.nomeRettangolo = nomeRettangolo;
+            this.baseRettangolo = baseRettangolo;
+            this.altezzaRettangolo = altezzaRettangolo;
+        }
+
+
+        public int CalcolaArea()
+        {
+            int area = baseRettangolo * altezzaRettangolo;
+            return area;
+        }
+
+        public int CalcolaPerimetro()
+        {
+            int perimetro = (baseRettangolo + altezzaRettangolo)*2;
+            return perimetro;
+        }
+
+        public void stampaRettangolo()
+        {
+            Console.WriteLine("——----" + nomeRettangolo + "——----");
+            Console.WriteLine("Base: " + baseRettangolo);
+            Console.WriteLine("Altezza: " + altezzaRettangolo);
+            Console.WriteLine("Perimetro: " + CalcolaPerimetro());
+            Console.WriteLine("Area: " + CalcolaArea());
+        }
+
+    }
+
